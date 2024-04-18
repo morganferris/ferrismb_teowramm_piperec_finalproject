c82f5cd2673abf739f432766486b23b6b9b796ce
teowramm <teowramm@vdi-LCB-IC-023.ad.uc.edu>
2024-04-18 13:36:43 -0400

    added the function to load the photo and code in the entry point to display it

--- ferrismb_teowramm_piperec_finalproject/decryptionPackage/decryption.py
+++ ferrismb_teowramm_piperec_finalproject/decryptionPackage/decryption.py
@@ -1,5 +1,8 @@
 import json
 from cryptography.fernet import Fernet
+from PIL import Image
+import os,sys
+from io import BytesIO
 
 
 
@@ -21,6 +24,22 @@ def decrypt_message(encrypted_message, key):
     f = Fernet(key)
     decrypted_message = f.decrypt(encrypted_message)
     return decrypted_message.decode('utf-8')
+
+def load_image(filename):
+    '''
+    load an image file from the disk
+    @param: the file name, the file to load
+    @return: the image object
+    '''
+    
+    try:
+        myimage = Image.open(filename)
+        myimage.load()
+    except:
+        
+        return None
+    return myimage
+    
 
 
 if __name__ == "__main__":
@@ -69,7 +88,7 @@ if __name__ == "__main__":
         "38919"
 
         ]
-    english_file = "C:/Users/18594/git/final/ferrismb_teowramm_piperec_finalproject/UCEnglish.txt"
+    english_file = "C:/Users/teowramm/git/ferrismb_teowramm_piperec_finalproject/ferrismb_teowramm_piperec_finalproject/UCEnglish.txt"
     decrypted_location = decrypt_location(encrypted_data, english_file)
     print("Decrypted location:", decrypted_location)
     
@@ -78,6 +97,16 @@ if __name__ == "__main__":
     encrypted_movie_name = b'gAAAAABlTNM6kuAuHJGUgPDEJcJX-feG_KgWzOGnZVsWMDDKGXjvuo7WgC8aAXO1oCJS7ItK-C6zHejJBJDLQ1OBbxZEoO-t5g=='
     decrypted_movie_name = decrypt_message(encrypted_movie_name, key)
     print("Decrypted Movie Name:", decrypted_movie_name)
+    
+    my_image = load_image("C:/Users/teowramm/git/ferrismb_teowramm_piperec_finalproject/ferrismb_teowramm_piperec_finalproject/FinalProject.jpeg")
+    if my_image:
+        my_image.show()
+    else:
+        print("Error: Failed to load the image.")
+
+    
+    
+    
 
     
 
